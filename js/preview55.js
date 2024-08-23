043a175fcf95165b7b589ef8e28c7b82ae475b2a
Julian <julianherbert.2007@gmail.com>
2024-08-23 16:18:14 -0400

    prepare for search update

--- js/preview55.js
+++ js/preview55.js
@@ -1,24 +1,24 @@
-//search box
+//search index
+
+let index = [];
+
+let children = document.querySelector(".container").children;
+for (let i=0; i < children.length; i++) {
+    let e = children[i];
+    if (e.classList.contains("row") && e != target.parentElement) {
+        let rowChildren = e.children;
+
+        for (let j=0; j < rowChildren.length; j++) {
+            let child = rowChildren[j];
+            index.push(child);
+        }
+    }
+}
 
 document.getElementById("search55").oninput = (e) => {
     let target = e.target;
     let search = e.target.value.trim().toLowerCase();
     
     let children = document.querySelector(".container").children;
-    for (let i=0; i < children.length; i++) {
-        let e = children[i];
-        if (e.classList.contains("row") && e != target.parentElement) {
-            let rowChildren = e.children;
-
-            for (let j=0; j < rowChildren.length; j++) {
-                let child = rowChildren[j];
-                let text = child.textContent.trim().toLowerCase();
-                if (text.includes(search)) {
-                    child.style.display = "";
-                } else {
-                    child.style.display = "none";
-                }
-            }
-        }
-    }
+    
 }
