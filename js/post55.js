489ec6f68c66a9c7b32816319c7bc21ddb2d9c0f
Julian <julianherbert.2007@gmail.com>
2024-08-23 16:12:35 -0400

    fixed iframe focus

--- js/post55.js
+++ js/post55.js
@@ -3,3 +3,9 @@
 document.getElementById("fullscreen55").onclick = (e) => {
     document.getElementById("iframe55").requestFullscreen();
 }
+
+function focusOnIframe() {
+    document.getElementById("iframe55").contentWindow.focus();
+}
+focusOnIframe();
+document.getElementById("iframe55").onclick = focusOnIframe;
