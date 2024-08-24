fe0b1f0317fcfb9ea25ef5462985e9b498c4b569
mochawoof <julianherbert.2007@gmail.com>
2024-08-24 17:37:18 -0400

    add game zoom

--- js/post55.js
+++ js/post55.js
@@ -5,8 +5,23 @@ let iframe55container = document.getElementById("iframe55container");
 
 document.getElementById("fullscreen55").onclick = (e) => {
     iframe55.requestFullscreen();
-    onResize();
 }
+
+//handle fullscreen transition
+
+let lastZoom = 1;
+
+iframe55.onfullscreenchange = (e) => {
+    if (document.fullscreenElement) {
+        lastZoom = getZoom();
+        setZoom(1);
+    } else {
+        setZoom(lastZoom);
+    }
+}
+
+
+//handle focus changes
 
 function focusOnIframe() {
     iframe55.contentWindow.focus();
@@ -20,3 +35,44 @@ iframe55.contentWindow.onfocus = (e) => {
 iframe55.contentWindow.onblur = (e) => {
     iframe55container.style.borderColor = "#fff";
 }
+
+//handle zooming
+
+function setZoom(zoom) {
+    if (isNaN(zoom)) {
+        zoom = 1;
+    }
+    iframe55.contentDocument.body.style.zoom = zoom;
+
+    //force iframe to acknowledge resize
+    iframe55.contentWindow.dispatchEvent(new Event("resize"))
+    return zoom;
+}
+
+function setZoomAndStore(zoom) {
+    setZoom(zoom);
+    localStorage.setItem("zoom55", zoom);
+}
+
+function getZoom() {
+    let zoom = parseFloat(iframe55.contentDocument.body.style.zoom);
+    if (isNaN(zoom)) {
+        return 1;
+    }
+    return zoom;
+}
+
+document.getElementById("zoom_in55").onclick = (e) => {
+    setZoomAndStore(getZoom() * 1.1);
+}
+document.getElementById("zoom_out55").onclick = (e) => {
+    setZoomAndStore(getZoom() * 0.9);
+}
+document.getElementById("zoom_reset55").onclick = (e) => {
+    setZoomAndStore(1);
+}
+
+//restore saved zoom on iframe load
+iframe55.onload = (e) => {
+    setZoom(localStorage.getItem("zoom55"));
+};
